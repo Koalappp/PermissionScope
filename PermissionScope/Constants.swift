54b7a878fe07cc5b33ee10692206a7d3bce6f757
Vivien Leroy <vivienleroy@me.com>
2015-12-21 15:34:52 +0100

    iOS 7 support + private overlapping

--- PermissionScope/Constants.swift
+++ PermissionScope/Constants.swift
@@ -10,22 +10,22 @@ import Foundation
 import UIKit
 
 enum Constants {
-    struct UI {
-        static let contentWidth: CGFloat                 = 280.0
-        static let dialogHeightSinglePermission: CGFloat = 260.0
-        static let dialogHeightTwoPermissions: CGFloat   = 360.0
-        static let dialogHeightThreePermissions: CGFloat = 460.0
-    }
-    
-    struct NSUserDefaultsKeys {
-        static let requestedInUseToAlwaysUpgrade = "PS_requestedInUseToAlwaysUpgrade"
-        static let requestedBluetooth            = "PS_requestedBluetooth"
-        static let requestedMotion               = "PS_requestedMotion"
-        static let requestedNotifications        = "PS_requestedNotifications"
-    }
-    
-    struct InfoPlistKeys {
-        static let locationWhenInUse             = "NSLocationWhenInUseUsageDescription"
-        static let locationAlways                = "NSLocationAlwaysUsageDescription"
-    } 
+	struct UI {
+		static let contentWidth: CGFloat                 = 280.0
+		static let dialogHeightSinglePermission: CGFloat = 260.0
+		static let dialogHeightTwoPermissions: CGFloat   = 360.0
+		static let dialogHeightThreePermissions: CGFloat = 460.0
+	}
+
+	struct NSUserDefaultsKeys {
+		static let requestedInUseToAlwaysUpgrade = "PS_requestedInUseToAlwaysUpgrade"
+		static let requestedBluetooth            = "PS_requestedBluetooth"
+		static let requestedMotion               = "PS_requestedMotion"
+		static let requestedNotifications        = "PS_requestedNotifications"
+	}
+
+	struct InfoPlistKeys {
+		static let locationWhenInUse             = "NSLocationWhenInUseUsageDescription"
+		static let locationAlways                = "NSLocationAlwaysUsageDescription"
+	}
 }
--- PermissionScope/Structs.swift
+++ PermissionScope/Structs.swift
@@ -10,61 +10,49 @@ import Foundation
 
 /// Permissions currently supportes by PermissionScope
 @objc public enum PermissionType: Int, CustomStringConvertible {
-    case Contacts, LocationAlways, LocationInUse, Notifications, Microphone, Camera, Photos, Reminders, Events, Bluetooth, Motion
-    
-    public var prettyDescription: String {
-        switch self {
-        case .LocationAlways, .LocationInUse:
-            return "Location"
-        default:
-            return "\(self)"
-        }
-    }
-    
-    public var description: String {
-        switch self {
-        case .Contacts:         return "Contacts"
-        case .Events:           return "Events"
-        case .LocationAlways:   return "LocationAlways"
-        case .LocationInUse:    return "LocationInUse"
-        case .Notifications:    return "Notifications"
-        case .Microphone:       return "Microphone"
-        case .Camera:           return "Camera"
-        case .Photos:           return "Photos"
-        case .Reminders:        return "Reminders"
-        case .Bluetooth:        return "Bluetooth"
-        case .Motion:           return "Motion"
-        }
-    }
-    
-    static let allValues = [Contacts, LocationAlways, LocationInUse, Notifications, Microphone, Camera, Photos, Reminders, Events, Bluetooth, Motion]
+	case Notifications
+
+	public var prettyDescription: String {
+		switch self {
+		default:
+			return "\(self)"
+		}
+	}
+
+	public var description: String {
+		switch self {
+		case .Notifications:    return "Notifications"
+		}
+	}
+
+	static let allValues = [Notifications]
 }
 
 /// Possible statuses for a permission.
 @objc public enum PermissionStatus: Int, CustomStringConvertible {
-    case Authorized, Unauthorized, Unknown, Disabled
-    
-    public var description: String {
-        switch self {
-        case .Authorized:   return "Authorized"
-        case .Unauthorized: return "Unauthorized"
-        case .Unknown:      return "Unknown"
-        case .Disabled:     return "Disabled" // System-level
-        }
-    }
+	case Authorized, Unauthorized, Unknown, Disabled
+
+	public var description: String {
+		switch self {
+		case .Authorized:   return "Authorized"
+		case .Unauthorized: return "Unauthorized"
+		case .Unknown:      return "Unknown"
+		case .Disabled:     return "Disabled" // System-level
+		}
+	}
 }
 
 /// Result for a permission status request.
 @objc public class PermissionResult: NSObject {
-    public let type: PermissionType
-    public let status: PermissionStatus
-    
-    internal init(type:PermissionType, status:PermissionStatus) {
-        self.type   = type
-        self.status = status
-    }
-    
-    override public var description: String {
-        return "\(type) \(status)"
-    }
+	public let type: PermissionType
+	public let status: PermissionStatus
+
+	internal init(type:PermissionType, status:PermissionStatus) {
+		self.type   = type
+		self.status = status
+	}
+
+	override public var description: String {
+		return "\(type) \(status)"
+	}
 }
